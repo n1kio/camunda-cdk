1963e20d4e0895025953da5aa8e781a20b17b6f3
Niklas Appelmann <niklasappelmann@gmail.com>
2022-02-02 23:50:13 +0100

    Db init

--- lib/camunda-cdk-stack.ts
+++ lib/camunda-cdk-stack.ts
@@ -1,16 +1,40 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as rds from '@aws-cdk/aws-rds';
+import * as ec2 from '@aws-cdk/aws-rds';
 
 export class CamundaCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const vpc = new ec2.Vpc(this, 'my-cdk-vpc', {
+      cidr: '10.0.0.0/16',
+    });
 
-    // The code that defines your stack goes here
+    const dbInstance = new rds.DatabaseInstance(this, 'DbInstance', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.ISOLATED,
+      },
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_13_1,
+      }),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO,
+      ),
+      credentials: rds.Credentials.fromGeneratedSecret('postgres'),
+      multiAz: false,
+      allocatedStorage: 100,
+      maxAllocatedStorage: 105,
+      allowMajorVersionUpgrade: false,
+      autoMinorVersionUpgrade: true,
+      backupRetention: cdk.Duration.days(0),
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
+      databaseName: `${this.stackName}Db`,
+      publiclyAccessible: false,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CamundaCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
